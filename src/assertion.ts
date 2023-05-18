2ad63bd5eefdeb75b2c15b9d0b5b2b1fbe9236d5
Mr. Hasan <hasanshahriar32@gmail.com>
2023-05-18 22:57:06 +0600

    updated

--- src/assertion.ts
+++ src/assertion.ts
@@ -1,4 +1,19 @@
-let emni;
+let emni: any;
 
 emni = "next level web dev";
 console.log(emni);
+console.log((emni as string).length);
+
+function kgToGram(param: string | number): string | number | undefined {
+  if (typeof param === "string") {
+    const value = parseFloat(param) * 1000;
+    return `the converted value is ${value} gram`;
+  }
+  if (typeof param === "number") {
+    const value = param * 1000;
+    return value;
+  }
+}
+
+const resultToBeNumber = kgToGram(1000);
+console.log(resultToBeNumber);
