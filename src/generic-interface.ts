1c42ad3e187c14740e73f10833b95b061da669aa
Mr. Hasan <hasanshahriar32@gmail.com>
2023-05-29 10:04:33 +0600

    added cocde4

--- src/generic-interface.ts
+++ src/generic-interface.ts
@@ -1,11 +1,19 @@
 interface CrushInterface<T> {
   name: string;
   husband: T;
+  //   salary: number
 }
 
 const crush1: CrushInterface<string> = {
   name: "kate",
   husband: "hasan",
+  //   salary: 4
 };
 
-// const crush2: CrushInterface;
+const crush2: CrushInterface<object> = {
+  name: "hasan",
+
+  husband: {
+    salary: 54,
+  },
+};
